8dd26165f31534926326325a28a8ec2b114b4efd
DinhThai03 <thai.05.10.2001@gmail.com>
2025-02-23 17:02:25 +0700

    sửa icon create

--- client/src/app/(dashboard)/list/teachers/page.tsx
+++ client/src/app/(dashboard)/list/teachers/page.tsx
@@ -89,7 +89,7 @@ const TeacherListPage = () => {
             // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
             //   <Image src="/delete.png" alt="" width={16} height={16} />
             // </button>
-            <FormModal table="teacher" type="delete" id={item.id}/>
+            <FormModal table="teacher" type="delete" id={item.id} />
           )}
         </div>
       </td>
@@ -111,10 +111,11 @@ const TeacherListPage = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-              // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              //   <Image src="/plus.png" alt="" width={14} height={14} />
-              // </button>
-              <FormModal table="teacher" type="create"/>
+              <>
+                <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                  <Image src="/plus.png" alt="" width={14} height={14} />
+                </button>
+              </>
             )}
           </div>
         </div>
