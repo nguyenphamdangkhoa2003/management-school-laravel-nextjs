0434185e34c21d6056b5922912cc689988d9e5fc
DinhThai03 <thai.05.10.2001@gmail.com>
2025-02-23 17:07:29 +0700

    sửa icon create parent

--- client/src/app/(dashboard)/list/parents/page.tsx
+++ client/src/app/(dashboard)/list/parents/page.tsx
@@ -83,7 +83,9 @@ const ParentListPage = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-              <FormModal table="teacher" type="create"/>
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                <Image src="/plus.png" alt="" width={14} height={14} />
+              </button>
             )}
           </div>
         </div>
